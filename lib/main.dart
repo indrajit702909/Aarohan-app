aa6142b6f00288acdcfee22a3cd278752eb98b6a
indrajit702909 <indrajitdasofficial.wb@gmail.com>
2024-03-31 17:46:37 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:aarohan/screens/signIn_screen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,16 +20,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Aarohan',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.deepOrange,
-        ).copyWith(secondary: Colors.deepOrange),
-        useMaterial3: true,
+    return Sizer(
+      builder: (context, orientation, deviceType) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Aarohan',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.deepOrange,
+          ).copyWith(secondary: Colors.deepOrange),
+          useMaterial3: true,
+        ),
+        home: const SplashScreen(),
       ),
-      home: const AuthPage(),
     );
   }
 }
@@ -53,6 +57,53 @@ class _AuthPageState extends State<AuthPage> {
           }
         },
       ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      duration: 2000,
+      splash: Container(
+        // color: Colors.black,
+        // height: 100.h,
+        // width: 1.w,
+        child: Column(
+          children: [
+            Container(
+              // height: 50.h,
+              // width: 50.w,
+              child: const Image(
+                image: AssetImage('assets/images/aarohan_logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'AAROHAN',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 20.sp,
+                fontFamily: 'Anurati',
+              ),
+            ),
+          ],
+        ),
+      ),
+      nextScreen: const AuthPage(),
+      backgroundColor: Color.fromARGB(255, 190, 81, 3),
+      splashTransition: SplashTransition.fadeTransition,
     );
   }
 }
